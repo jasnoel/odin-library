9de70185769f1e429cbf118f65032058e7c531af
jasnoel <jason.noel.imp@gmail.com>
2022-04-06 01:14:36 +0200

    created book object and addbooktolibrary function

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("hI!")
+let myLibrary = [];
+
+function Book(title, author, bookLenght) {
+    this.title = title;
+    this.author = author;
+    this.bookLenght = bookLenght;
+    this.read = false;
+}
+
+Book.prototype.infos = function() {
+    return `${this.title} by ${this.author}, ${this.bookLenght}, ${this.read ? "read":"not read yet"}`
+}
+
+function addBookToLibrary() {
+    let title = prompt("Title");
+    let author = prompt("Author");
+    let bookLenght = prompt("Number of pages")
+    let newBook = new Book(title, author, bookLenght);
+    myLibrary.push(newBook);
+}
